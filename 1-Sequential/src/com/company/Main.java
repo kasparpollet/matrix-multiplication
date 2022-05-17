28f12780a6cd4cc29d68c5dc4bd9a9bd7e7c35f9
Kaspar Pollet <kaspar@Kaspars-Air.home>
2022-05-17 16:49:32 +0200

    random generation

--- 1-Sequential/src/com/company/Main.java
+++ 1-Sequential/src/com/company/Main.java
@@ -1,60 +1,63 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 
-// TODO use FloatingPoint
 // TODO potential dataset: https://www.kaggle.com/datasets/fivethirtyeight/uber-pickups-in-new-york-city?select=uber-raw-data-jul14.csv
 public class Main {
 
     public static void main(String[] args) {
-        int r1,r2,c1,c2,i,j,k,sum;
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter the number of rows of matrix1");
-        r1 = in.nextInt();
+        System.out.println("Enter the size for the two matrices, it will be used for the columns and rows");
+        int size = in.nextInt();
 
-        System.out.println("Enter the number columns of matrix 1");
-        c1 = in.nextInt();
-        System.out.println("Enter the number of rows of matrix2");
-        r2 = in.nextInt();
+        float[][] mat1 = generateMatrix(size);
+        float[][] mat2 = generateMatrix(size);
 
-        System.out.println("Enter the number of columns of matrix 2");
-        c2 = in.nextInt();
+        long start = System.currentTimeMillis();
+        float[][] res = multiplyMatrices(mat1, mat2);
+        long end = System.currentTimeMillis();
+        long time = end-start;
 
-        if(c1==r2) {
-            int[][] mat1 = new int[r1][c1];
-            int[][] mat2 = new int[r2][c2];
-            int[][] res = new int[r1][c2];
+        System.out.printf("\nIt took: \n%d ms, \n%f sec", time, time / Math.pow(10, 3));
+    }
 
-            System.out.println("Enter the elements of matrix1");
-
-            for ( i= 0 ; i < r1 ; i++ ) {
-                for ( j= 0 ; j < c1 ;j++ )
-                    mat1[i][j] = in.nextInt();
-            }
-            System.out.println("Enter the elements of matrix2");
-
-            for ( i= 0 ; i < r2 ; i++ ) {
-                for ( j= 0 ; j < c2 ;j++ )
-                    mat2[i][j] = in.nextInt();
-            }
-
-            System.out.println("\n\noutput matrix:-");
-            for ( i= 0 ; i < r1 ; i++ )
-                for ( j= 0 ; j <c2;j++) {
-                    sum = 0;
-                    for ( k= 0 ; k <r2;k++ ) {
-                        sum += mat1[i][k]*mat2[k][j] ;
-                    }
-                    res[i][j] = sum;
-                }
-            for ( i= 0 ; i < r1; i++ ) {
-                for ( j=0 ; j < c2;j++ )
-                    System.out.print(res[i][j]+" ");
-                System.out.println();
+    private static float[][] generateMatrix(int size) {
+        Random r = new Random();
+        float[][] matrix = new float[size][size];
+        for(int i=0; i<size; i++)
+        {
+            for(int j=0; j<size; j++)
+            {
+                matrix[i][j] = r.nextInt(Integer.MAX_VALUE) + r.nextFloat();
             }
         }
-        else System.out.print("multipication does not exist ");
+        return matrix;
+    }
+
+    private static float[][] multiplyMatrices(float[][] mat1, float[][] mat2){
+        float[][] res = new float[mat1.length][mat2.length];
+        float sum;
+
+        for (int i=0 ; i < res.length ; i++ )
+            for (int j=0 ; j < res.length; j++) {
+                sum = 0;
+                for (int k= 0 ; k < res.length; k++) {
+                    sum += mat1[i][k]*mat2[k][j] ;
+                }
+                res[i][j] = sum;
+            }
+        return res;
+    }
+
+    private static void printMatrix(float[][] matrix) {
+        for (float[] floats : matrix) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(floats[j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
